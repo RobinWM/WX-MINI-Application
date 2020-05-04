aca4b5545acd3d6d335a1d1fb7507534bc4873c8
Robin <302740969@qq.com>
2020-05-04 20:02:00 +0800

    '小程序初体验'

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,22 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Robin',
+    age: 18,
+    students: [
+      { id: 1, name: 'Lily', age: 19 },
+      { id: 2, name: 'Lala', age: 18 },
+      { id: 3, name: 'ming', age: 20 },
+    ],
+    counter: 0
   },
+  handleBtnClick(){
+    // 错误做法，页面不会刷新
+    // this.data.counter++;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    this.setData({
+      counter:this.data.counter+1
+    });
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    console.log(this.data.counter);
+  }  
 })
